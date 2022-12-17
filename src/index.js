c6486ab3e644edc1f6977d62f3de397b3b7e7001
tareq fleyfel <tareq.fleyfel@gmail.com>
2022-12-17 19:59:14 +0300

    minor changes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ import React, { StrictMode } from "react";
 import ReactDom from "react-dom/client";
 
 import App from "./App";
-
+import App2 from "./App2";
 const root = ReactDom.createRoot(document.getElementById("root"));
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+root.render(<App />);
